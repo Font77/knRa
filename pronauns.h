3c89e167fa1ec9f653362d5cfbc2ce61b5bd6c2e
viml qumar <viml@vnindia.in>
2017-11-06 16:29:08 +0530

    Create pronauns.h

---
+++ pronauns.h
@@ -0,0 +1,19 @@
+// http://mylanguages.org/kannada_pronouns.php
+I speak	-> nanu mathanaduthini
+you speak	-> ninu mathanaduthiya
+he speaks	-> avanu mathanaduthiddane
+she speaks	avalu mathanaduthaailddale
+we speak	naavu mathanaduthiddeve
+they speak	avaru mathanaduthiddare
+give me	nanage kodi
+ninage kodutheene
+give him	avanige kodu
+give her	avalige kodu
+give us	namage kodu
+give them	avarige kodu
+my book	nanna pustaka
+your book	ninna pustaka
+his book	avana pustaka
+her book	avala pustaka
+our book	namma pustaka
+their book	avara pustaka
